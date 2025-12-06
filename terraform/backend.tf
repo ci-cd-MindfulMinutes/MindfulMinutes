d39e4dc51e6a07d984e63aaa92f47699d187b53b
nixacfilms <nikhil.nixaclabs@gmail.com>
2025-12-06 18:14:46 -0500

    Added a project prefix for managing terraform state

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,13 +1,15 @@
 # Terraform state backend configuration
 # This stores your Terraform state file remotely in S3
-# You need to manually create this S3 bucket first before running terraform init
+# Terraform workspaces automatically separate dev/prod states:
+# - Dev: env:/dev/terraform.tfstate
+# - Prod: env:/prod/terraform.tfstate
 
 terraform {
   backend "s3" {
     bucket         = "mindfulminutes-terraform-state"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock"
+    dynamodb_table = "mindfulminutes-terraform-state-lock"
     encrypt        = true
   }
 }
