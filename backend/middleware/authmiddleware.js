54a686e1b897006a32893fed92db0a94dc79e4fc
nixacfilms <nikhil.nixaclabs@gmail.com>
2025-12-07 03:56:01 -0500

    Redeploying backend

--- backend/middleware/authmiddleware.js
+++ backend/middleware/authmiddleware.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const authMiddleware = (req, res, next) => {
     const token = req.header('Authorization')
     if (!token) {
-        return res.status(401).json({ message: 'Access denied. No token provided.' })
+        return res.status(401).json({ message: 'Access denied. No token provided..' })
     }
     try {
         const decoded = jwt.verify(token.replace('Bearer ', ''), process.env.JWT_SECRET)
